3daa5529bd5add7ec8c53bd5d97609570d4bc70a
Jesus11-web <80785736+Jesus11-web@users.noreply.github.com>
2021-04-19 09:51:27 -0500

    Heroes y Listado Heroe

---
+++ src/app/Heroes/heroe/heroe.component.ts
@@ -0,0 +1,32 @@
+import { UpperCasePipe } from '@angular/common';
+import {Component, OnInit} from '@angular/core';
+
+@Component  ({
+    selector: 'app-heroe',
+    templateUrl: './heroe.component.html',
+    styleUrls: ['./heroe.component.css'],
+})
+
+export class HeroeComponent{
+
+    nombre: string = 'Saitama';
+    edad: number = 30;
+
+    obtenerNombre(): string{
+        return this.nombre + ' - ' + this.edad;  
+    }
+
+    cambiarNombre(): void{
+        this.nombre = 'Superman';
+    }
+
+    cambiarEdad(): void{
+        this.edad = 38;
+    }
+
+    get nombreMayuscula(): string{
+        return this.nombre.toUpperCase();
+    }
+
+    
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HeroeComponent } from './Heroes/heroe/heroe.component';
+import { ListadoComponent } from './Heroes/listado/listado.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+     HeroeComponent,
+     ListadoComponent,
   ],
+
   imports: [
     BrowserModule,
     AppRoutingModule
