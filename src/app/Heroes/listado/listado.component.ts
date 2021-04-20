9b7d9032f6274b1dcb2655604cbe8197fa45895c
Jesus11-web <80785736+Jesus11-web@users.noreply.github.com>
2021-04-20 11:26:35 -0500

    Listado Heroes Borrados

--- src/app/Heroes/listado/listado.component.ts
+++ src/app/Heroes/listado/listado.component.ts
@@ -1,16 +1,23 @@
-import { Component} from '@angular/core';
+import { removeSummaryDuplicates, ThrowStmt, splitClasses } from '@angular/compiler';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
+
   selector: 'app-listado',
   templateUrl: './listado.component.html',
   styleUrls: ['./listado.component.css']
 })
 export class ListadoComponent{
 
-  heroes: string[] = ['hulk','saitama','superman','goku'];
-  
+  heroes: string[] = ['hulk','SAITAMA','SUPERMAN','goku'];
+eliminar: string[]  = []
+indice:number = 0;
   removeItem(i: number){
-    this.heroes.splice(i, 1);
+
+    this.indice=this.indice+i;
+    this.eliminar = this.heroes.splice(i,1);
+
+
   }
 
 }
